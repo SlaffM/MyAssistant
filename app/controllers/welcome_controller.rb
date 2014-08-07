24af92d69493d0dfcbbef0c7d7c5201c1aef1e43
Vyacheslav Modzhuk <pochtos@yandex.ru>
2014-08-07 23:54:33 +0300

    add shedule listing by user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+
+
+    @shedules = Shedule.where(user_id: current_user.id)
+
+
+
+    # @shedules.as_json.each do |i|
+    #   p i["type_shedule"]
+    # end
+
   end
 end
