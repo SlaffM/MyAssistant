07165c9fdf91c575a1306e00647253c516871fba
Vyacheslav <pochtos@yandex.ru>
2014-08-07 15:49:17 +0400

    Revert "add spec for users, shedulers"

--- app/models/shedule.rb
+++
@@ -1,3 +0,0 @@
-class Shedule < ActiveRecord::Base
-  belongs_to :user
-end
--- spec/factories/shedules.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :shedule do
-    type_shedule "Train"
-    favorite_town_to "Moscow"
-    favorite_town_from "Khimki"
-  end
-end
