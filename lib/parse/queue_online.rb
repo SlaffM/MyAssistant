21daa8ffd9c5326f2bb98e17da8a1b4645a1c653
Vyacheslav Modzhuk <pochtos@yandex.ru>
2015-03-26 21:12:12 +0200

    queue_online work

--- lib/parse/queue_online.rb
+++ lib/parse/queue_online.rb
@@ -2,10 +2,10 @@ class QueueOnline
   require 'open-uri'
 
   def get_html
-    url = 'http://er.zdravmo.ru/registratu/3/Himki_gorodskoi_okrug/khimkinskaya_detskaya_gorodskaya_poliklinika_1_pediatricheskoe_otdelenie/'
+    #url = 'http://er.zdravmo.ru/registratu/3/Himki_gorodskoi_okrug/khimkinskaya_detskaya_gorodskaya_poliklinika_1_pediatricheskoe_otdelenie/'
 
-    #root = Rails.root.to_s
-    #url = './lib/parse/site/site.html'
+    root = Rails.root.to_s
+    url = './lib/parse/site/site.html'
     page = open(url)
     h = Hash.new
 
