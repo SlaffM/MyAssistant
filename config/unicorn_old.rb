da5a08695d9b5dc0aec96e4c80e45d1015c45e2f
Vyacheslav Modzhuk <pochtos@yandex.ru>
2015-04-09 22:11:40 +0300

    capistrano deploy:setup

---
+++ config/unicorn_old.rb
@@ -0,0 +1,28 @@
+#config/unicorn_old.rb
+
+# Set the working application directory
+# working_directory '/path/to/your/app'
+working_directory '/var/www/apps/MyAssistant'
+
+# Unicorn PID file location
+# pid '/path/to/pids/unicorn.pid'
+pid '/var/www/apps/MyAssistant/pids/unicorn.pid'
+
+# Path to logs
+# stderr_path '/path/to/log/unicorn.log'
+# stdout_path '/path/to/log/unicorn.log'
+stderr_path '/var/www/apps/MyAssistant/log/unicorn.log'
+stdout_path '/var/www/apps/MyAssistant/log/unicorn.log'
+
+# Unicorn socket
+# listen '/tmp/unicorn.[application name].sock'
+listen '/tmp/unicorn.MyAssistant.sock'
+listen 8080, :tcp_nopush => true
+
+
+# Number of processes
+# worker_processes 4
+worker_processes 2
+
+# Time-out
+timeout 30
