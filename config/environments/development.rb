f24cc0215ed61ff0719bc748cc4746c9932d5241
SlaffM <slavkamm@gmail.com>
2015-04-15 22:40:35 +0300

    deploy changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
   config.assets.precompile += %w(applicatiom-*.js)
+  config.assets.compile = true
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
